2d3bbd79be5e2d433169bb85f0d5daf8132855a6
SEEZUS44 <szwe031@gmail.com>
2019-12-11 14:04:19 +0200

    longest day at work

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,12 +2,16 @@
 
 @section('content')
 <H1>create post</H1>
-<form method="post" action="/posts">
+{{-- <form method="post" action="/posts"> --}}
     {{-- The above stpulates the method that'll be used by the form. --}}
     {{-- using the artisan routes created --}}
     {{-- you then access the method posts store which is in PostsController@store --}}
 
-    <input type="text" name="title" placeholder="Enter Title:">
+
+    {!! Form::open() !!}
+
+    <input type="text" name="title" placeholder="Enter Title">
+
     {{csrf_field()}}
     <input type="submit" value="submit">
     {{-- Most of the inner workings are within the PostsController
